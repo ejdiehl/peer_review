04c3915a391be7c782462d5f8a54acdcca77fabf
gallegretti <gabriel13082@hotmail.com>
2016-05-20 14:55:57 -0300

    Filling Conference and University classes

--- src/peer_review/models/Conference.java
+++ src/peer_review/models/Conference.java
@@ -1,5 +1,59 @@
 package peer_review.models;
 
-public class Conference {
+import java.util.ArrayList;
 
+public class Conference {
+	private String initials;
+	private ArrayList<Article> articlesSubmitted;
+	private ArrayList<Article> articlesAllocated;
+	private ArrayList<Researcher> committeeMembers;
+	private Researcher coordinator;
+	
+	public Conference(String initials, ArrayList<Article> articlesSubmitted, ArrayList<Article> articlesAllocated, ArrayList<Researcher> committeeMembers, Researcher coordinator) {
+		this.initials = initials;
+		this.articlesSubmitted = articlesSubmitted;
+		this.articlesAllocated = articlesAllocated;
+		this.committeeMembers = committeeMembers;
+		this.coordinator = coordinator;
+	}
+	
+	public int getLowestSubmittedArticleID() {
+		// TODO: Implement
+		return -1;
+	}
+	
+	public ArrayList<Researcher> getCandidateReviewers(Article article) {
+		// TODO: Implement
+		return null;
+	}
+	
+	public ArrayList<Researcher> sortReviewers(ArrayList<Researcher> researchCandidates) {
+		// TODO: Implement
+		return null;
+	}
+	
+	public Article allocateArticle(Article lowestIdSubmittedArticle, Researcher firstSortedResearcher ) {
+		// TODO: Implement
+		return null;
+	}
+	
+	public ArrayList<Article> getAcceptedArticles() {
+		// TODO: Implement
+		return null;
+	}
+	
+	public ArrayList<Article> getRejectedArticles() {
+		// TODO: Implement
+		return null;
+	}
+	
+	public Researcher getCoordinator() {
+		// TODO: Implement
+		return null;
+	}
+	
+	public boolean hasUnreviewedArticles() {
+		// TODO: Implement
+		return false;
+	}
 }
--- src/peer_review/models/University.java
+++ src/peer_review/models/University.java
@@ -1,5 +1,13 @@
 package peer_review.models;
 
 public class University {
-
+	private String name;
+	
+	public University(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
 }
