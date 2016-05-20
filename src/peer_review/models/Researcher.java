c0e13d9cd384fa24202788d05ff2cc852faf4e36
gallegretti <gabriel13082@hotmail.com>
2016-05-20 14:21:39 -0300

    Sync lab work - Eduardo

--- src/peer_review/models/Researcher.java
+++ src/peer_review/models/Researcher.java
@@ -1,5 +1,40 @@
 package peer_review.models;
 
-public class Researcher {
+import java.util.ArrayList;
 
+public class Researcher {
+	private int id;
+	private String name;
+	private University affiliation;
+	private ArrayList<ResearchTopic> researchTopic;
+	public ArrayList<Article> allocatedArticles;
+	
+	public ArrayList<ResearchTopic> getResearchTopics(){
+		return researchTopic;
+	}
+	
+	public University getUniversity(){
+		return affiliation;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public int getID(){
+		return id;
+	}
+	
+	public void allocateArticle(Article article){
+		// TODO: Implement
+	}
+	
+	public Researcher(int id, String name, University affiliation, ArrayList<ResearchTopic> researchTopic, ArrayList<Article> allocatedArticles){
+		this.id = id;
+		this.name = name;
+		this.affiliation = affiliation;
+		this.researchTopic = researchTopic;
+		this.allocatedArticles = allocatedArticles;
+		
+	}
 }
