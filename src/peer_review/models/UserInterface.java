d173369597666df1fd37adac434da31c2e36bf3f
rrrichter <guigo_richter@hotmail.com>
2016-05-24 21:26:36 -0300

    Created userInterface

---
+++ src/peer_review/models/UserInterface.java
@@ -0,0 +1,53 @@
+package peer_review.models;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class UserInterface {
+	
+	public void getCommand() {
+		
+	}
+	
+	public void showMessage(String message) {
+		
+	}
+	
+	public String readString() {
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine();
+		return s;
+	}
+	
+	public int readInteger() {
+		Scanner scan = new Scanner(System.in);
+		int i = scan.nextInt();
+		return i;
+	}
+	
+	public float readFloat() {
+		Scanner scan = new Scanner(System.in);
+		float f = scan.nextFloat();
+		return f;
+	}
+	
+	public void showUI() {
+		
+	}
+	
+	public void showArticlesList() {
+		
+	}
+	
+	public void showArticleReviewersList(Article article) {
+		
+	}
+	
+	public void showConferences() {
+		
+	}
+
+	public void showArticlesWithGrades(ArrayList<Article> articlesList) {
+	
+	}
+}
